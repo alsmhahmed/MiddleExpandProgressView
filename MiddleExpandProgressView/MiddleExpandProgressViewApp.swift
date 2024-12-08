7f3f677a357089832ff927169704948cd460279f
alsmhahmed <alsmh.ahmed@gen-c.com>
2024-12-08 13:26:25 +0200

    Initial Commit

---
+++ MiddleExpandProgressView/MiddleExpandProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiddleExpandProgressViewApp.swift
+//  MiddleExpandProgressView
+//
+//  Created by alsmh ahmed on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MiddleExpandProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
